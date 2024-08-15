102bf61b137fe4a36bca81fac6f54c7ce292df93
Ion Dodon <iondodon2@gmail.com>
2024-08-15 13:53:31 +0300

    title bar black color

--- .config/polybar/scripts/info-java.sh
+++ .config/polybar/scripts/info-java.sh
@@ -1,16 +1,10 @@
 #!/bin/bash
 
-# Check if sdkman is installed
-if [ -z "$SDKMAN_DIR" ]; then
-  echo "sdkman is not installed. Please install sdkman first."
-  exit 1
-fi
-
 # Load sdkman
-source "$SDKMAN_DIR/bin/sdkman-init.sh"
+source "/home/ion/.sdkman/bin/sdkman-init.sh"
 
 # Get the current Java version
 current_java_version=$(sdk current java | grep -oP '(?<=Using java version ).*')
 
-# Print the current Java version with the Java Unicode character
+# Print the current Java version
 echo -e "Java $current_java_version"

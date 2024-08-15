95ac057f6f81ee6cd3cdd3b31ceb27ce3a4073f8
Ion Dodon <iondodon2@gmail.com>
2024-08-15 14:14:03 +0300

    dbeaver

--- .config/i3/startup.sh
+++ .config/i3/startup.sh
@@ -22,6 +22,7 @@ sleep 2
 i3-msg "workspace $ws2"
 #i3-msg "exec code"
 i3-msg "exec /home/ion/.local/share/JetBrains/Toolbox/apps/intellij-idea-community-edition/bin/idea.sh"
+i3-msg "exec dbeaver"
 i3-msg "layout tabbed"
 i3-msg "[workspace=$ws2] focus child"
 

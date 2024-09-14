fe1ae8bc5303e8eb81e34cab32ce583e4d97c654
Ion Dodon <iondodon2@gmail.com>
2024-09-14 18:58:56 +0300

    updated startup

--- .config/awesome/startup.sh
+++ .config/awesome/startup.sh
@@ -2,16 +2,17 @@
 
 # Start applications on specific workspaces
 
-awesome-client 'run_on_tag("1", "google-chrome")'
 awesome-client 'run_on_tag("1", "firefox")'
+sleep 1
+awesome-client 'run_on_tag("1", "google-chrome")'
 
 sleep 2
 
 awesome-client 'awful.tag.find_by_name(awful.screen.focused(), "2"):view_only()'
-awesome-client 'run_on_tag("2", "dbeaver")'
 awesome-client 'run_on_tag("2", "/home/ion/.local/share/JetBrains/Toolbox/apps/intellij-idea-community-edition/bin/idea.sh")'
+awesome-client 'run_on_tag("2", "dbeaver")'
 
-sleep 10
+sleep 5
 
 awesome-client 'awful.tag.find_by_name(awful.screen.focused(), "3"):view_only()'
 # awesome-client 'run_on_tag("3", "terminator --layout=magentus")'
@@ -27,7 +28,6 @@ sleep 2
 
 awesome-client 'awful.tag.find_by_name(awful.screen.focused(), "5"):view_only()'
 awesome-client 'run_on_tag("5", "cherrytree")'
-awesome-client 'run_on_tag("5", "outlook-for-linux")'
 
 sleep 1
 

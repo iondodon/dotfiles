5ba23c1843d9beb58aae7dcc99f5c07fc0899112
Ion Dodon <iondodon2@gmail.com>
2024-09-22 12:08:03 +0300

    removed pomodoro

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -724,7 +724,6 @@ client.connect_signal("unfocus", function(c) c.border_color = beautiful.border_n
 -- Autostart applications
 awful.spawn.with_shell("xinput set-prop \"VEN_0488:00 0488:1024 Touchpad\" \"libinput Tapping Enabled\" 1")
 
-awful.spawn.with_shell("~/scripts/pomodoro.sh")
 awful.spawn.with_shell("compton")
 awful.spawn.with_shell("nm-applet")
 awful.spawn.with_shell("nitrogen --restore")

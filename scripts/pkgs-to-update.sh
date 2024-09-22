c58322839c3454e02f6e5003d25ec87309faa12d
Ion Dodon <iondodon2@gmail.com>
2024-09-22 17:33:40 +0300

    apt

--- scripts/pkgs-to-update.sh
+++ scripts/pkgs-to-update.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 
-# Get the number of packages that can be upgraded
-num_updates=$(aptitude search '~U' | wc -l)
+# Update the package list, ignoring all output
+# sudo apt update > /dev/null 2>&1
+
+# Get the number of upgradable packages
+upgradable_count=$(apt list --upgradable 2>/dev/null | grep -v "^Listing" | wc -l)
 
 # Check if the number of updates is greater than 0
-if [ "$num_updates" -gt 0 ]; then
+if [ "$upgradable_count" -gt 0 ]; then
     # If there are updates, make the arrow green
-     echo "<span foreground=\"green\" font_size=\"10.0pt\">⇑</span>$num_updates"
+    echo "<span foreground=\"green\" font_size=\"10.0pt\">⇑</span>$upgradable_count"
 else
     # If no updates, make the arrow a neutral color (e.g., gray or keep it unchanged)
-    echo "<span font_size=\"10.0pt\">⇑</span>$num_updates"
+    echo "<span font_size=\"10.0pt\">⇑</span>$upgradable_count"
 fi
 

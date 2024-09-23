55ba52b289e7362f8f85b3c9628560834cbb6b4a
Ion Dodon <iondodon2@gmail.com>
2024-09-23 16:43:13 +0300

    use zenburn

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -51,7 +51,7 @@ end
 
 -- {{{ Variable definitions
 -- Themes define colours, icons, font and wallpapers.
-beautiful.init(gears.filesystem.get_configuration_dir() .. "themes/default/theme.lua")
+beautiful.init(gears.filesystem.get_configuration_dir() .. "themes/zenburn/theme.lua")
 
 -- This is used later as the default terminal and editor to run.
 terminal = "kitty"

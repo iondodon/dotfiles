a953e2da1dca9088941fb1d7a04af91d60cb42d0
Ion Dodon <iondodon2@gmail.com>
2024-10-01 10:32:39 +0300

    next prev cline

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -374,20 +374,20 @@ globalkeys = gears.table.join(
     awful.key({ modkey,           }, "Escape", awful.tag.history.restore,
               {description = "go back", group = "tag"}),
 
-    awful.key({ modkey,           }, "Right",
+    awful.key({ modkey,           }, "w",
         function ()
             awful.client.focus.byidx( 1)
         end,
         {description = "focus next by index", group = "client"}
     ),
-    awful.key({ modkey,           }, "Left",
+    awful.key({ modkey,           }, "q",
         function ()
             awful.client.focus.byidx(-1)
         end,
         {description = "focus previous by index", group = "client"}
     ),
-    awful.key({ modkey,           }, "w", function () mymainmenu:show() end,
-              {description = "show main menu", group = "awesome"}),
+    --awful.key({ modkey,           }, "w", function () mymainmenu:show() end,
+      --        {description = "show main menu", group = "awesome"}),
 
     -- Layout manipulation
     awful.key({ modkey, "Shift"   }, "Right", function () awful.client.swap.byidx(  1)    end,

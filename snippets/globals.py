1bf4419e24fb58ccc25b6a0068a149efbbdfc69d
Ion Dodon <iondodon2@gmail.com>
2024-11-02 13:49:01 +0200

    snippet generate uuid v4

--- snippets/globals.py
+++ snippets/globals.py
@@ -1,10 +1,15 @@
 from datetime import datetime, timezone
+import uuid
 
 def get_current_datetime_iso8601_utc():
 	return datetime.now(timezone.utc).isoformat()
+
+def generate_uuid_v4():
+    return str(uuid.uuid4())
  
 
 globals = {
     "name": "Mike Barkmin",
-    "get_current_datetime_iso8601_utc": get_current_datetime_iso8601_utc
+    "get_current_datetime_iso8601_utc": get_current_datetime_iso8601_utc,
+    "generate_uuid_v4": generate_uuid_v4
 }

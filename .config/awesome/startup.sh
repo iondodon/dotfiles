0635ad2ee8dd8af1370789353770863b7d1147fb
Ion Dodon <iondodon2@gmail.com>
2024-11-14 17:16:37 +0200

    kitty kdocker icon

--- .config/awesome/startup.sh
+++ .config/awesome/startup.sh
@@ -8,5 +8,5 @@ awesome-client 'awful.util.spawn("outlook-for-linux")'
 awesome-client 'awful.util.spawn("/home/ion/.local/share/JetBrains/Toolbox/apps/intellij-idea-community-edition/bin/idea.sh")'
 
 # awesome-client 'awful.util.spawn_with_shell("kdocker kitty --session ~/.config/kitty/magentus")'
-awesome-client 'awful.util.spawn("kdocker kitty")'
+awesome-client 'awful.util.spawn("kdocker -i /home/ion/kitty/share/icons/hicolor/256x256/apps/kitty.png kitty")'
 

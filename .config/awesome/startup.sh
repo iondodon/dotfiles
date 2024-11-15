9f94778676e8d51751393ec644fc24e472450427
Ion Dodon <iondodon2@gmail.com>
2024-11-15 13:58:04 +0200

    kitty

--- .config/awesome/startup.sh
+++ .config/awesome/startup.sh
@@ -7,6 +7,5 @@ awesome-client 'awful.util.spawn("teams-for-linux")'
 awesome-client 'awful.util.spawn("outlook-for-linux")'
 awesome-client 'awful.util.spawn("/home/ion/.local/share/JetBrains/Toolbox/apps/intellij-idea-community-edition/bin/idea.sh")'
 
-# awesome-client 'awful.util.spawn("kdocker kitty --session ~/.config/kitty/magentus")'
 awesome-client 'awful.util.spawn("kdocker -i /home/ion/.local/kitty.app/share/icons/hicolor/256x256/apps/kitty.png kitty")'
 

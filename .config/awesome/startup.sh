4afd5a9eeb2016bc4f183d8498e1af89dce21fa4
Ion Dodon <iondodon2@gmail.com>
2024-11-21 16:30:23 +0200

    startup slack

--- .config/awesome/startup.sh
+++ .config/awesome/startup.sh
@@ -4,6 +4,7 @@
 
 awesome-client 'awful.util.spawn("google-chrome")'
 awesome-client 'awful.util.spawn("teams-for-linux")'
+awesome-client 'awful.util.spawn("slack --startup --silent")'
 awesome-client 'awful.util.spawn("outlook-for-linux")'
 awesome-client 'awful.util.spawn("/home/ion/.local/share/JetBrains/Toolbox/apps/intellij-idea-community-edition/bin/idea.sh")'
 

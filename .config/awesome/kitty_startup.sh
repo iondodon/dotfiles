5d8d2ff5eaf780bed7dd87072ec9b3f1a9215f88
Ion Dodon <iondodon2@gmail.com>
2024-11-25 15:58:36 +0200

    kitty

--- .config/awesome/kitty_startup.sh
+++ .config/awesome/kitty_startup.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 kitty --title KittyGlobal --override confirm_os_window_close=1 &
-kdocker -n KittyGlobal
+kdocker -n KittyGlobal -i ~/.local/kitty.app/share/icons/hicolor/256x256/apps/kitty.png

59441774ca89a4e0b4bc2363d321822c06c6a7df
Ion Dodon <iondodon2@gmail.com>
2024-12-02 17:59:01 +0200

    cherrytree

--- .config/awesome/startup.sh
+++ .config/awesome/startup.sh
@@ -8,5 +8,7 @@ awesome-client 'awful.util.spawn("slack --startup --silent")'
 awesome-client 'awful.util.spawn("outlook-for-linux")'
 awesome-client 'awful.util.spawn("/home/ion/.local/share/JetBrains/Toolbox/apps/intellij-idea-community-edition/bin/idea.sh")'
 
-awesome-client 'awful.util.spawn("/home/ion/.config/awesome/kitty_startup.sh")'
+awesome-client 'awful.util.spawn_with_shell("kitty --title KittyGlobal --override confirm_os_window_close=1 & kdocker -n KittyGlobal -i ~/.local/kitty.app/share/icons/hicolor/256x256/apps/kitty.png")'
+
+awesome-client 'awful.util.spawn_with_shell("kdocker cherrytree")'
 

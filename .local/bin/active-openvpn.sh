4d904f4817be7bf57acb04b144280cc0c58646f7
Ion Dodon <iondodon2@gmail.com>
2026-02-03 16:09:32 +0200

    scripts cleanup

--- .local/bin/active-openvpn.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Check if an OpenVPN client is active by looking for the specific OpenVPN command
-if pgrep -f "openvpn3 session-start --config" > /dev/null; then
-  echo "<span foreground=\"green\" font_size=\"large\">◉︎</span>VPN"
-else
-  echo "<span font_size=\"large\">◉︎</span>VPN"
-fi
-
--- .local/bin/active-openvpn3.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Check if an OpenVPN client session is active
-if openvpn3 sessions-list | grep -q "Path:"; then
-  echo "<span foreground=\"green\" font_size=\"large\">◉︎</span>VPN"
-else
-  echo "<span font_size=\"large\">◉︎</span>VPN"
-fi
